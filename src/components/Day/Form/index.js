a93dc115e7851be3ff3b13dbbc3ca4e431579b5b
Angelica Acosta <angelicapaolaacosta@gmail.com>
2019-07-15 21:23:35 -0500

    calendar-challenge

---
+++ src/components/Day/Form/index.js
@@ -0,0 +1,74 @@
+import React from "react";
+import TimePicker from "rc-time-picker";
+import ColorPicker from "rc-color-picker";
+import moment from "moment";
+import "rc-time-picker/assets/index.css";
+import "rc-color-picker/assets/index.css";
+import "./index.css";
+
+const reminderForm = props => {
+  const time = props.reminder.time
+    ? moment(props.reminder.time, "HH:mm a")
+    : moment()
+        .hour(0)
+        .minute(0);
+
+  return (
+    <form
+      method="post"
+      onSubmit={e => props.handleCreateUpdateReminder(e, props.reminder)}
+    >
+      <textarea
+        className="description"
+        placeholder="enter event description..."
+        maxLength="30"
+        defaultValue={props.reminder.description}
+      />
+
+      <select
+        value={props.reminder.state || ""}
+        onChange={props.handleEventState}
+      >
+        <option value="">City...</option>
+        <option value="New York">New York</option>
+        <option value="New Jersey">New Jersey</option>
+        <option value="Buenos Aires">Buenos Aires</option>
+        <option value="Bogota">Bogota</option>
+        <option value="London">London</option>
+        <option value="Tokyo">Tokyo</option>
+      </select>
+
+      <label>Weather: </label>
+      <label>{props.reminder.temperature}</label>
+
+      <TimePicker
+        showSecond={false}
+        defaultValue={time}
+        format="h:mm a"
+        use12Hours
+        inputReadOnly
+      />
+
+      <span>Reminder color:</span>
+      <ColorPicker
+        className="color-picker"
+        animation="slide-up"
+        color={props.reminder.color || props.defaultColor}
+        onClose={props.handleSetColor}
+      />
+
+      <div className="buttons">
+        <button className="btn-submit">Submit</button>
+
+        <button
+          className="btn-cancel"
+          onClick={() => props.handleSetEditDay(null)}
+        >
+          Cancel
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default reminderForm;
---
+++ src/components/Day/Reminder/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./index.css";
+
+const reminder = props => (
+  <article className="reminder" style={{ background: props.reminder.color }}>
+    <div className="tools">
+      <button onClick={() => props.handleDeleteReminder(props.reminder.id)}>
+        <i className="fas fa-trash-alt" />
+      </button>
+      <button onClick={() => props.handleSetEdit(props.reminder)}>
+        <i className="fas fa-edit" />
+      </button>
+    </div>
+    <strong>{props.reminder.description}</strong>
+    <strong>{props.reminder.state}</strong>
+    <strong>{props.reminder.temperature}°F</strong>
+    <time>{props.reminder.time}</time>
+  </article>
+);
+
+export default reminder;
